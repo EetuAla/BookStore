4b78eaf0f718d308dd8756a66ae8367789aa4d0d
EetuAla <ezka.alanen@gmail.com>
2023-09-12 15:48:20 +0300

    Added delete button, for deleting books.

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.Bookstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,5 +32,10 @@ public class BookController {
 		repository.save(book);
 		return "redirect:/booklist";
 	}
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long id, Model model) {
+    	repository.deleteById(id);
+        return "redirect:../booklist";
+    }     
 
 }
