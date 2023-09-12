775a8f2c3a7f9af25356d4949da3d3ac64f762ce
EetuAla <ezka.alanen@gmail.com>
2023-09-12 15:34:47 +0300

    Added 'add' button for adding more books to the table

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -4,19 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
-	@RequestMapping(value= {"/", "/booklist"})
-    public String bookList(Model model) {	
-        model.addAttribute("books", repository.findAll());
-        return "booklist";
-    }
-  
+
+	@RequestMapping(value = { "/", "/booklist" })
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+
+	@RequestMapping("/add")
+	public String showAddBookForm(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:/booklist";
+	}
+
 }
-
-
